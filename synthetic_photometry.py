21d5e4141728a958a1fa054b56ea16cf0e95acaf
Genaro <gsuarez@astro.unam.mx>
2024-05-24 18:38:08 -0400

    All from SVO

--- synthetic_photometry.py
+++ synthetic_photometry.py
@@ -22,35 +22,37 @@ def synthetic_photometry(wl, flux, filters, flux_unit, eflux=None):
 
 	Returns
 	------
-	out_synthetic_photometry: python dictionary with the following parameters for each filter
-		out_synthetic_photometry['lambda_eff(um)']: effective wavelength in micron
-		out_synthetic_photometry['width_eff(um)']: effective width in micron
-		out_synthetic_photometry['syn_flux(erg/s/cm2/A)']: synthetic flux in erg/s/cm2/A
-		out_synthetic_photometry['esyn_flux(erg/s/cm2/A)']: synthetic flux errors in erg/s/cm2/A (if input flux errors are provided)
-		out_synthetic_photometry['syn_flux(Jy)']: synthetic flux in Jy
-		out_synthetic_photometry['esyn_flux(Jy)']: synthetic flux errors in Jy (if input flux errors are provided)
-		out_synthetic_photometry['syn_mag']: synthetic magnitude
-		out_synthetic_photometry['esyn_mag']: synthetic magnitude error (if input flux errors are provided)
+	out: python dictionary with the following parameters for each filter
+		out['lambda_eff(um)']: effective wavelength in micron
+		out['width_eff(um)']: effective width in micron
+		out['syn_flux(erg/s/cm2/A)']: synthetic flux in erg/s/cm2/A
+		out['esyn_flux(erg/s/cm2/A)']: synthetic flux errors in erg/s/cm2/A (if input flux errors are provided)
+		out['syn_flux(Jy)']: synthetic flux in Jy
+		out['esyn_flux(Jy)']: synthetic flux errors in Jy (if input flux errors are provided)
+		out['syn_mag']: synthetic magnitude
+		out['esyn_mag']: synthetic magnitude error (if input flux errors are provided)
 
 	Example
-	>>> import synthetic_photometry as synthetic_photometry 
-	>>> # assume we have a spectrum wavelength (wl in um), flux (in erg/s/cm2/A), and flux error (eflux) and we want synthetic photometry for several filters
+	>>> import synthetic_photometry
+	>>> # assume we have a spectrum wavelength (wl in um), flux (in erg/s/cm2/A), and flux error (eflux) 
+	>>>	# and we want synthetic photometry for several filters
 	>>> filters = (['Spitzer/IRAC.I1', 'WISE/WISE.W1']) # filters of interest
 	>>>	# run the code
-	>>> out_synthetic_photometry = synthetic_photometry.synthetic_photometry(wl=wl, flux=flux, eflux=eflux, flux_unit='erg/s/cm2/A', filters=filters)
+	>>> out = synthetic_photometry.synthetic_photometry(wl=wl, flux=flux, eflux=eflux, flux_unit='erg/s/cm2/A', filters=filters)
 	>>> # output
-	>>> eff_wl = out_synthetic_photometry['lambda_eff(um)'] # effective wavelength (um) for all filters
-	>>> eff_width = out_synthetic_photometry['width_eff(um)'] # effective width (um) for all filters
-	>>> flux_syn = out_synthetic_photometry['syn_flux(erg/s/cm2/A)'] # synthetic flux (erg/s/cm2/A) for all filters
-	>>> eflux_syn = out_synthetic_photometry['esyn_flux(erg/s/cm2/A)'] # synthetic flux errors (erg/s/cm2/A) for all filters
-	>>> flux_Jy_syn = out_synthetic_photometry['syn_flux(Jy)'] # synthetic flux (Jy) for all filters
-	>>> eflux_Jy_syn = out_synthetic_photometry['esyn_flux(Jy)'] # synthetic flux errors (Jy) for all filters
-	>>> mag_syn = out_synthetic_photometry['syn_mag'] # synthetic magnitude for all filters
-	>>> emag_syn = out_synthetic_photometry['esyn_mag'] # synthetic magnitude error for all filters
+	>>> eff_wl = out['lambda_eff(um)'] # effective wavelength (um) for all filters
+	>>> eff_width = out['width_eff(um)'] # effective width (um) for all filters
+	>>> flux_syn = out['syn_flux(erg/s/cm2/A)'] # synthetic flux (erg/s/cm2/A) for all filters
+	>>> eflux_syn = out['esyn_flux(erg/s/cm2/A)'] # synthetic flux errors (erg/s/cm2/A) for all filters
+	>>> flux_Jy_syn = out['syn_flux(Jy)'] # synthetic flux (Jy) for all filters
+	>>> eflux_Jy_syn = out['esyn_flux(Jy)'] # synthetic flux errors (Jy) for all filters
+	>>> mag_syn = out['syn_mag'] # synthetic magnitude for all filters
+	>>> emag_syn = out['esyn_mag'] # synthetic magnitude error for all filters
 
 	Author: Genaro Suárez
 
 	Modification history
+		2024/05/24	SVO VOTable table is read from a link with the most updated filter parameters
 		2024/05/09	included VOTable table with all SVO filter zero points
 		2024/05/07	filter transmissions are read and downloaded from SVO, if not already stored locally
 		2024/04/20	function created
